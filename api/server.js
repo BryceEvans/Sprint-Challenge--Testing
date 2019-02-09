bef3f0e5e83e496bddeb86b59a5ea1bce1411e34
BryceEvans <evans.bryce@gmail.com>
2019-02-09 10:57:24 -0700

    one end point written and one test passing.

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const games = require('../games/gamesModel');
 const server = express();
 
 server.get('/', (req, res) => {
-  
+  res.status(200).json({ api: 'running' });
 })
 
 module.exports = server;
