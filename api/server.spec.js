f1278a3d61d8b2d3bdc393bc7c16cec8ee77274b
BryceEvans <evans.bryce@gmail.com>
2019-02-09 11:14:02 -0700

    three more tests written for get /games

--- api/server.spec.js
+++ api/server.spec.js
@@ -22,14 +22,17 @@ describe('get / route handler', () => {
   })
 })
 
-// describe('', () => {
-//   it('', async () => {
-
-//   })
-//   it('', async () => {
-
-//   })
-//   it('', async () => {
-
-//   })
-// })
+describe('/get games', () => {
+  it('responds with 200', async () => {
+    const response = await request(server).get('/games');
+    expect(response.status).toBe(200);
+  })
+  it('responds with json', async () => {
+    const response = await request(server).get('/games');
+    expect(response.type).toMatch(/json/i);
+  })
+  it('semds cprrect response object', async () => {
+    const response = await request(server).get('/games');
+    expect(response.body).toEqual([]);
+  })
+})
